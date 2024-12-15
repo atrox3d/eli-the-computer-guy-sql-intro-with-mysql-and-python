f19eed0842adb215014d001ba976f14b5452fe90
atrox3d <atrox3d@gmail.com>
2024-12-15 09:29:56 +0100

    insert data

---
+++ 01-insert-data.sql
@@ -0,0 +1,17 @@
+use company;
+
+insert into client
+    (name, age, shirt_size)
+values
+    ('bob', 23, 'large');
+
+insert into client
+    (name, age, shirt_size)
+values
+    ('sue', 14, 'large'),
+    ('sam', 44, 'medium'),
+    ('bill', 54, 'medium'),
+    ('fred', 50, 'small'),
+    ('tammy', 50, 'small');
+
+
